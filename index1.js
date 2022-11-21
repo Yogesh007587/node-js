bfc0f44776a6d47956d8fcc6eba654ae48939757
Yogesh007587 <yogeshtripathi2002@gmail.com>
2022-11-22 01:45:10 +0530

    fundamentals of node js

--- index1.js
+++ index1.js
@@ -9,6 +9,13 @@ var x='20';
 //     console.log(i);
 // }
 const arr=[2,3,4,5,5,6,7,];
+
+let result=arr.filter((item)=>{
+// console.log(item)
+    return item>=4
+})
+console.warn(result);
 // console.log(arr);
-console.log(index2.x);
-console.log(index2.y);
+// console.log(index2.x);
+// console.log(index2.y);
+// console.log(index2.z())
