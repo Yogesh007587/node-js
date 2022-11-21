2bd24652b911aa8b3cb8d0a72003811abf1a24b9
Yogesh007587 <yogeshtripathi2002@gmail.com>
2022-11-22 01:38:14 +0530

    start

---
+++ index1.js
@@ -0,0 +1,14 @@
+const index2 = require('./index2')
+
+
+var x='20';
+// if(x==20){
+    // console.log("matched");
+// }
+// for(i=0;i<10;i++){
+//     console.log(i);
+// }
+const arr=[2,3,4,5,5,6,7,];
+// console.log(arr);
+console.log(index2.x);
+console.log(index2.y);
