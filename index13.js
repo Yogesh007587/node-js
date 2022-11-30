c18baf7612a6cbd684498dcc36295990221447a3
Yogesh007587 <yogeshtripathi2002@gmail.com>
2022-11-30 23:21:07 +0530

    express js

---
+++ index13.js
@@ -0,0 +1,17 @@
+const { application } = require('express');
+const express =require('express');
+const app =express();
+app.get('',(req,res)=>{
+    res.send('Hello Kakarot, Welcome to home')
+});
+
+app.get('/about',(req,res)=>{
+    res.send('Hello Kakarot, Lets go to about')
+});
+app.get('/help',(req,res)=>{
+    res.send('Hello kakarot,help me');
+});
+app.get('/game',(req,res)=>{
+    res.send('Lets play game')
+});
+app.listen(4222);
